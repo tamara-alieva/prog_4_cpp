de5d9ef8b0623dbb11150e930f139cc4e4e39f94
Tamara <tamaraavgust14@gmail.com>
2023-12-03 16:45:51 +0700

    Добавление класса Person

---
+++ Person.h
@@ -0,0 +1,12 @@
+class Person {
+private:
+	string name;
+	int balance;
+public:
+	void setName(string name);
+	void setBalance(int balance);
+	string getName();
+	int getBalance();
+	void input();
+	void output();
+};
