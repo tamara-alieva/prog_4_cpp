d2325fc34c547cc356b49d83541fe471f03be701
Tamara <tamaraavgust14@gmail.com>
2023-12-03 16:56:20 +0700

    Добавление класса

---
+++ Passenger.h
@@ -0,0 +1,10 @@
+class Passenger : public Person{
+private:
+	bool payment_method;
+public:
+	void setMethod(bool method);
+	bool getMethod();
+	void input();
+	void output();
+	void takePayment(int payment);
+};
