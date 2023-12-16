76c8185c7340eeede6cf91ef3e8c5a0d7a7d086c
Tamara <tamaraavgust14@gmail.com>
2023-12-17 02:54:16 +0700

    Инициализация статического поля

--- main/main.cpp
+++ main/main.cpp
@@ -1,2 +1,11 @@
-#include <iostream>
+#include "../Car.h"
+#include "../Driver.h"
+#include "../Fuel.h"
+#include "../Passenger.h"
+#include "../Person.h"
 
+int Car::numberOfCars = 0; // инициализация статического поля
+
+int main() {
+    
+}
